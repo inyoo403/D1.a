2cf0af31df0211b3f20aec6bb27be8a35a6ae497
inyoo403 <inyoo@ucsc.edu>
2025-10-10 10:41:26 +0000

    Step 2: Add counter that increases when clicked

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import "./style.css";
 
+let apples = 0;
+
 const app = document.createElement("div");
 app.className = "app";
 document.body.appendChild(app);
@@ -8,11 +10,17 @@ const title = document.createElement("h1");
 title.textContent = "🍎 Apple Clicker";
 app.appendChild(title);
 
+const counter = document.createElement("p");
+counter.className = "stats";
+counter.textContent = `🍎 ${apples}`;
+app.appendChild(counter);
+
 const mainBtn = document.createElement("button");
 mainBtn.className = "main-btn";
 mainBtn.textContent = "🍎";
 app.appendChild(mainBtn);
 
 mainBtn.addEventListener("click", () => {
-  console.log("Button clicked!");
+  apples += 1;
+  counter.textContent = `🍎 ${apples}`;
 });
